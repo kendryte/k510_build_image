06750b8a40f5d279a03a1b0045564153388544d6
GongT <admin@gongt.me>
2022-03-10 16:41:54 +0800

    nncase update to 1.4

--- ci/nncase.sh
+++ ci/nncase.sh
@@ -2,9 +2,8 @@
 
 set -Eeuo pipefail
 
-tar xf nncase_v1.1.0.tgz
+tar xf nncase_v1.4.0.tgz
 
-mv nncase_v1.1.0/x86_64/* ./
+mv nncase_v1.4.0/x86_64/* ./
 
-rm -rf nncase_v1.1.0 nncase_v1.1.0.tgz
-
+rm -rf nncase_v1.4.0 nncase_v1.4.0.tgz
